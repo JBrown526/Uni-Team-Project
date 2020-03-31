fcd875f31c5d92123b36410ac181396bb6aade00
JBrown526 <45610774+JBrown526@users.noreply.github.com>
2020-03-31 22:41:35 +0100

    Setup for CommissionRateAdd

--- src/ats/pages/guis/financial/CommissionRateAdd.java
+++ src/ats/pages/guis/financial/CommissionRateAdd.java
@@ -1,11 +1,18 @@
 package ats.pages.guis.financial;
 
+import ats.pages.Page;
+
 import javax.swing.*;
 
-public class CommissionRateAdd {
+public class CommissionRateAdd extends Page {
     private JPanel mainPanel;
     private JButton backButton;
     private JButton logoutButton;
 
     //TODO: Create This
+
+    @Override
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
 }
