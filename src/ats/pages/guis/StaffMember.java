8e8ed7b7b5f61e80340ab09ee6dd8b63266145da
JBrown526 <45610774+JBrown526@users.noreply.github.com>
2020-03-31 15:10:06 +0100

    Completed update staff member details functionality

--- src/ats/pages/guis/StaffMember.java
+++ src/ats/pages/guis/StaffMember.java
@@ -4,9 +4,8 @@ import ats.App;
 import ats.pages.TablePage;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.Optional;
 
 public class StaffMember extends TablePage {
     int staffID;
@@ -45,10 +44,20 @@ public class StaffMember extends TablePage {
             mainPanel.remove(adminPanel);
         }
 
-        applyButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //TODO: Change user details functionality
+        applyButton.addActionListener(e -> {
+            if (isEmpty(roleField.getText()) || validRole(roleField.getText())) {
+                // ensures at least one field has been filled
+                if (!isEmpty(roleField.getText()) || !isEmpty(nameField.getText())
+                        || !isEmpty(String.valueOf(passwordField.getPassword()))
+                        || !isEmpty(phoneNumberField.getText()) || !isEmpty((emailField.getText()))
+                        || !isEmpty(addressField.getText()) || !isEmpty(cityField.getText())
+                        || !isEmpty(postcodeField.getText())) {
+                    updateStaffMember();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Please fill in new details");
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "That is an invalid user role");
             }
         });
 
@@ -69,6 +78,99 @@ public class StaffMember extends TablePage {
                 try (ResultSet rs = ps.executeQuery()) {
                     blankTable.setModel(buildTableModel(rs));
                 }
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
+
+    private static boolean isEmpty(String fieldValue) {
+        return fieldValue.equals("");
+    }
+
+    private boolean validRole(String role) {
+        boolean validRole = false;
+
+        try (Connection conn = DriverManager.getConnection(credentials[0], credentials[1], credentials[2])) {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT role_code FROM role WHERE role_code = ?;")) {
+                ps.setString(1, role);
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        validRole = true;
+                    }
+                }
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return validRole;
+    }
+
+    private static String removeLastCharacter(String str) {
+        return Optional.ofNullable(str)
+                .filter(sStr -> sStr.length() != 0)
+                .map(sStr -> sStr.substring(0, sStr.length() - 1))
+                .orElse(str);
+    }
+
+    private static int setStatementValue(PreparedStatement ps, String str, int i) throws SQLException {
+        if (!isEmpty(str)) {
+            ps.setString(i, str);
+            i++;
+        }
+        return i;
+    }
+
+    private void updateStaffMember() {
+        String newRole = roleField.getText();
+        String newName = nameField.getText();
+        String newPassword = String.valueOf(passwordField.getPassword());
+        String newPhoneNumber = phoneNumberField.getText();
+        String newEmail = emailField.getText();
+        String newAddress = addressField.getText();
+        String newCity = cityField.getText();
+        String newPostCode = postcodeField.getText();
+
+        String sqlUpdate = "";
+
+        sqlUpdate += isEmpty(newRole) ? "" : " role_code = ?,";
+        sqlUpdate += isEmpty(newName) ? "" : " name = ?,";
+        sqlUpdate += isEmpty(newPassword) ? "" : " password = ?,";
+        sqlUpdate += isEmpty(newPhoneNumber) ? "" : " phone_number = ?,";
+        sqlUpdate += isEmpty(newEmail) ? "" : " email = ?,";
+        sqlUpdate += isEmpty(newAddress) ? "" : " address = ?,";
+        sqlUpdate += isEmpty(newCity) ? "" : " city = ?,";
+        sqlUpdate += isEmpty(newPostCode) ? "" : " postcode = ?,";
+        sqlUpdate = removeLastCharacter(sqlUpdate);
+
+        String sql = String.format("UPDATE ats.staff SET%s WHERE staff_id = ?;", sqlUpdate);
+
+        try (Connection conn = DriverManager.getConnection(credentials[0], credentials[1], credentials[2])) {
+            try (PreparedStatement ps = conn.prepareStatement(sql)) {
+                int insertPosition = 1;
+
+                insertPosition = setStatementValue(ps, newRole, insertPosition);
+                insertPosition = setStatementValue(ps, newName, insertPosition);
+                insertPosition = setStatementValue(ps, newPassword, insertPosition);
+                insertPosition = setStatementValue(ps, newPhoneNumber, insertPosition);
+                insertPosition = setStatementValue(ps, newEmail, insertPosition);
+                insertPosition = setStatementValue(ps, newAddress, insertPosition);
+                insertPosition = setStatementValue(ps, newCity, insertPosition);
+                insertPosition = setStatementValue(ps, newPostCode, insertPosition);
+
+                ps.setInt(insertPosition, staffID);
+                ps.executeUpdate();
+
+                roleField.setText("");
+                nameField.setText("");
+                passwordField.setText("");
+                phoneNumberField.setText("");
+                emailField.setText("");
+                addressField.setText("");
+                cityField.setText("");
+                postcodeField.setText("");
+
+                populateTable();
             }
         } catch (SQLException sqle) {
             sqle.printStackTrace();
