ad2b6704649f076ea03fcf445f1afde7caee5cf2
JBrown526 <45610774+JBrown526@users.noreply.github.com>
2020-04-01 13:23:29 +0100

    Basic setup for CommissionRateAdd

--- src/ats/pages/guis/financial/CommissionRateAdd.java
+++ src/ats/pages/guis/financial/CommissionRateAdd.java
@@ -1,15 +1,51 @@
 package ats.pages.guis.financial;
 
+import ats.App;
+import ats.common.Utilities;
 import ats.pages.Page;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class CommissionRateAdd extends Page {
+    int staffID;
+    String[] credentials;
+
     private JPanel mainPanel;
     private JButton backButton;
     private JButton logoutButton;
+    private JComboBox typeSelectBox;
+    private JTextField commissionRateField;
+    private JButton setRateButton;
+    private JTextField dateField;
 
     //TODO: Create This
+
+    public CommissionRateAdd(App app, int staffID) {
+        this.staffID = staffID;
+        credentials = app.getDBCredentials();
+        Utilities.fillTypeDropdown(credentials, typeSelectBox, "commission");
+
+        setRateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        backButton.addActionListener(e -> app.toCommissionRates(staffID));
+        logoutButton.addActionListener(e -> app.logout());
+
+        commissionRateField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent ke) {
+                restrictInputToPositiveFloat(ke, commissionRateField, "Commission Rate");
+            }
+        });
+    }
 
     @Override
     public JPanel getMainPanel() {
