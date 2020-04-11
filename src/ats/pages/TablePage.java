ccb9eeaaf606debe773bb55aa257779729c7bf52
JBrown526 <45610774+JBrown526@users.noreply.github.com>
2020-04-11 20:20:55 +0100

    Populating table in StockTurnover

--- src/ats/pages/TablePage.java
+++ src/ats/pages/TablePage.java
@@ -12,31 +12,30 @@ public abstract class TablePage extends Page {
 
     // Generates a TableModel from a ResultSet
     public static DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
-        ResultSetMetaData metaData = null;
-            metaData = rs.getMetaData();
+        ResultSetMetaData metaData = rs.getMetaData();
 
-            // names of columns
-            Vector<String> columnNames = new Vector<>();
-            int columnCount = metaData.getColumnCount();
-            for (int column = 1; column <= columnCount; column++) {
-                columnNames.add(metaData.getColumnName(column));
+        // names of columns
+        Vector<String> columnNames = new Vector<>();
+        int columnCount = metaData.getColumnCount();
+        for (int column = 1; column <= columnCount; column++) {
+            columnNames.add(metaData.getColumnName(column));
+        }
+
+        // data of the table
+        Vector<Vector<Object>> data = new Vector<>();
+        while (rs.next()) {
+            Vector<Object> vector = new Vector<>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                vector.add(rs.getObject(columnIndex));
             }
+            data.add(vector);
+        }
 
-            // data of the table
-            Vector<Vector<Object>> data = new Vector<>();
-            while (rs.next()) {
-                Vector<Object> vector = new Vector<>();
-                for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-                    vector.add(rs.getObject(columnIndex));
-                }
-                data.add(vector);
+        return new DefaultTableModel(data, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
             }
-
-            return new DefaultTableModel(data, columnNames) {
-                @Override
-                public boolean isCellEditable(int row, int column) {
-                    return false;
-                }
-            };
+        };
     }
 }
--- src/ats/pages/guis/reports/StockTurnover.java
+++ src/ats/pages/guis/reports/StockTurnover.java
@@ -4,11 +4,18 @@ import ats.App;
 import ats.pages.TablePage;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.Vector;
 
 public class StockTurnover extends TablePage {
+    private String[] credentials;
     private boolean managerView;
+
+    Vector<String> columnNames;
+    Vector<Vector<Object>> data;
 
     private JPanel mainPanel;
     private JButton backButton;
@@ -18,10 +25,19 @@ public class StockTurnover extends TablePage {
     private JButton viewIndividualTurnoverButton;
     private JButton resetViewButton;
     private JComboBox staffMemberComboBox;
+    private JButton generateReportButton;
+    private JTextField dateField;
 
     public StockTurnover(App app, boolean managerView) {
         //TODO: this
         this.managerView = managerView;
+        credentials = app.getDBCredentials();
+
+        columnNames = new Vector<>();
+        data = new Vector<>();
+
+        populateTable();
+        showResults();
 
         viewIndividualTurnoverButton.addActionListener(new ActionListener() {
             @Override
@@ -29,12 +45,7 @@ public class StockTurnover extends TablePage {
 
             }
         });
-        resetViewButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
+        resetViewButton.addActionListener(e -> app.toStockTurnover(managerView));
 
         backButton.addActionListener(e -> {
             if (managerView) {
@@ -53,6 +64,76 @@ public class StockTurnover extends TablePage {
 
     @Override
     protected void populateTable() {
+        try (Connection conn = DriverManager.getConnection(credentials[0], credentials[1], credentials[2])) {
+            try (PreparedStatement ps = conn.prepareStatement(
+                    "SELECT CONCAT(MIN(blank_id), '-', MAX(blank_id)) AS 'FROM/TO BLANK NBRS', COUNT(blank_id) AS 'AMNT' " +
+                            "FROM blank WHERE blank_status = 'AVBL' AND date_received BETWEEN '2019-04-03' AND '2019-07-03' " +
+                            "GROUP BY blank_type;\n")) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    makeTable(rs);
+                }
+            }
+            try (PreparedStatement ps = conn.prepareStatement(
+                    "SELECT staff_id AS 'CODE', CONCAT(MIN(blank_id), '-', MAX(blank_id)) AS 'FROM/TO BLANKS NBRS', COUNT(blank_id) AS 'AMNT'\n" +
+                            "        FROM blank WHERE blank_status = 'ASGN' AND date_received BETWEEN '2019-04-03' AND '2019-07-03' AND date_assigned BETWEEN '2019-04-03' AND '2019-07-03'\n" +
+                            "        GROUP BY blank_type, staff_id;")) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    makeTable(rs);
+                }
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
 
+    private void makeTable(ResultSet rs) throws SQLException {
+        ResultSetMetaData metaData = rs.getMetaData();
+
+        int columnCount = metaData.getColumnCount();
+        for (int column = 1; column <= columnCount; column++) {
+            columnNames.add(metaData.getColumnName(column));
+        }
+
+        while (rs.next()) {
+            Vector<Object> vector = new Vector<>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                vector.add(rs.getObject(columnIndex));
+            }
+            data.add(vector);
+        }
+    }
+
+    private void showResults() {
+        DefaultTableModel dtm = new DefaultTableModel(data, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        stockTurnoverTable.setModel(dtm);
     }
 }
+
+//    SELECT CONCAT(MIN(blank_id), '-', MAX(blank_id)) AS 'FROM/TO BLANK NBRS', COUNT(blank_id) AS 'AMNT'
+//        FROM blank WHERE blank_status = 'AVBL' AND date_received BETWEEN '2019-04-03' AND '2019-07-03'
+//        GROUP BY blank_type;
+//
+//        SELECT staff_id AS 'CODE', CONCAT(MIN(blank_id), '-', MAX(blank_id)) AS 'FROM/TO BLANKS NBRS', COUNT(blank_id) AS 'AMNT'
+//        FROM blank WHERE blank_status = 'ASGN' AND date_received BETWEEN '2019-04-03' AND '2019-07-03' AND date_assigned BETWEEN '2019-04-03' AND '2019-07-03'
+//        GROUP BY blank_type, staff_id;
+//
+//        SELECT staff_id AS 'CODE', CONCAT(MIN(blank_id), '-', MAX(blank_id)) AS 'ASSIGNED (FROM/TO)', COUNT(blank_id) AS 'AMNT'
+//        FROM blank WHERE blank_status = 'ASGN' AND date_received NOT BETWEEN '2019-04-03' AND '2019-07-03' AND date_assigned BETWEEN '2019-04-03' AND '2019-07-03'
+//        GROUP BY blank_type, staff_id;
+//
+//        SELECT CONCAT(MIN(blank_id), '-', MAX(blank_id)) AS 'USED (FROM/TO)', COUNT(blank_id) AS 'AMNT'
+//        FROM blank WHERE blank_status = 'SOLD' AND date_sold BETWEEN '2019-04-03' AND '2019-07-03'
+//        GROUP BY blank_type;
+//
+//        SELECT 'CODE', CONCAT(MIN(blank_id), '-', MAX(blank_id)) AS 'TOTAL AVAILABLE (FROM/TO)', COUNT(blank_id) AS 'AMNT'
+//        FROM blank WHERE blank_status = 'AVBL'
+//        GROUP BY blank_type;
+//
+//        SELECT staff_id AS 'CODE', CONCAT(MIN(blank_id), '-', MAX(blank_id)) AS 'TOTAL ASSIGNED (FROM/TO)', COUNT(blank_id) AS 'AMNT'
+//        FROM blank WHERE blank_status = 'ASGN'
+//        GROUP BY blank_type, staff_id;
