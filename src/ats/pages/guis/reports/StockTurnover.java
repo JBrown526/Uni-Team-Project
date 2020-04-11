747de715d714333399f177e89f372ecf4135aa0c
JBrown526 <45610774+JBrown526@users.noreply.github.com>
2020-04-11 01:57:15 +0100

    Setup for StockTurnover

--- src/ats/pages/guis/reports/StockTurnover.java
+++ src/ats/pages/guis/reports/StockTurnover.java
@@ -1,4 +1,19 @@
 package ats.pages.guis.reports;
 
-public class StockTurnover {
+import ats.pages.TablePage;
+
+import javax.swing.*;
+
+public class StockTurnover extends TablePage {
+    private JPanel mainPanel;
+
+    @Override
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    @Override
+    protected void populateTable() {
+
+    }
 }
