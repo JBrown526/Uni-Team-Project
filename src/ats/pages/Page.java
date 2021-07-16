c0b3fc7a2b2126c8d8860cb7e44177aa8f7b0c35
Jamie Brown <jamiejbrown521@gmail.com>
2021-07-16 02:02:13 +0100

    Update Page.java

--- src/ats/pages/Page.java
+++ src/ats/pages/Page.java
@@ -11,6 +11,7 @@ public abstract class Page {
 
     public abstract JPanel getMainPanel();
 
+    // only allows integer inputs in a field
     public static void restrictInputToPositiveInt(KeyEvent ke, JTextField field, String fieldName) {
         if ((ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9') || ke.getExtendedKeyCode() == KeyEvent.VK_BACK_SPACE) {
             field.setEditable(true);
@@ -20,6 +21,7 @@ public abstract class Page {
         }
     }
 
+    // only allows float input in a field
     public static void restrictInputToPositiveFloat(KeyEvent ke, JTextField field, String fieldName) {
         if ((ke.getKeyChar() >= '0' && ke.getKeyChar() <= '9') || ke.getExtendedKeyCode() == KeyEvent.VK_BACK_SPACE
                 || ke.getExtendedKeyCode() == KeyEvent.VK_PERIOD) {
@@ -30,10 +32,12 @@ public abstract class Page {
         }
     }
 
+    // checks if a string is a valid date
     public static boolean isValidDate(String d) {
         return DATE_PATTERN.matcher(d).matches();
     }
 
+    // checks if the status of a blank fits the desired status
     public static boolean isStatus(String[] credentials, String blankID, String desiredStatus) {
         try (Connection conn = DriverManager.getConnection(credentials[0], credentials[1], credentials[2])) {
             try (PreparedStatement ps = conn.prepareStatement("SELECT blank_status FROM blank WHERE blank_id = ?")) {
@@ -49,18 +53,24 @@ public abstract class Page {
         return false;
     }
 
+    // returns the sql for an insert person query based on the provided fields
     protected static String populatePersonInsertQuery(String table, String[] sqlFragments, JTextField phoneNumberField,
                                                       JTextField emailField, JTextField addressField, JTextField cityField,
                                                       JTextField postcodeField) {
+        // creates a string of sql fields that need updating and their corresponding values
+        // sqlFragments[0] and sqlFragments[1] hold the fields that are mandatory to update and their values to update
         String sqlFields = sqlFragments[0];
         String sqlUpdate = sqlFragments[1];
 
+        // gets values of person information from their fields
         String phoneNumber = phoneNumberField.getText();
         String email = emailField.getText();
         String address = addressField.getText();
         String city = cityField.getText();
         String postcode = postcodeField.getText();
 
+        // adds the field to be updated if the field form was not empty
+        // adds the value from the field to the update sql if it is not empty
         sqlFields += Utilities.isEmpty(phoneNumber) ? "" : " phone_number,";
         sqlUpdate += Utilities.isEmpty(phoneNumber) ? "" : " '" + phoneNumber + "',";
         sqlFields += Utilities.isEmpty(email) ? "" : " email,";
@@ -72,14 +82,17 @@ public abstract class Page {
         sqlFields += Utilities.isEmpty(postcode) ? "" : " postcode,";
         sqlUpdate += Utilities.isEmpty(postcode) ? "" : " '" + postcode + "',";
 
+        // rmoves the last character so there are no trailing commas
         sqlFields = Utilities.removeLastCharacter(sqlFields);
         sqlUpdate = Utilities.removeLastCharacter(sqlUpdate);
 
         String sql;
+        // creates an sql query for inserting a person to the database
         sql = String.format("INSERT INTO %s (%s) VALUES (%s);", table, sqlFields, sqlUpdate);
         return sql;
     }
 
+    // executes an update to the database from a prepared statement and resets the input of any populated fields to blank
     protected static void commonInsertStatement(String[] credentials, String sql, JTextField[] fields) {
         try (Connection conn = DriverManager.getConnection(credentials[0], credentials[1], credentials[2])) {
             try (PreparedStatement ps = conn.prepareStatement(sql)) {
@@ -95,6 +108,7 @@ public abstract class Page {
         }
     }
 
+    // date format matching Regex
     private static final Pattern DATE_PATTERN = Pattern.compile(
             "^((2000|2400|2800|(19|2[0-9](0[48]|[2468][048]|[13579][26])))-02-29)$"
                     + "|^(((19|2[0-9])[0-9]{2})-02-(0[1-9]|1[0-9]|2[0-8]))$"
